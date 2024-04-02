e821462207fd1c71c8c48591d7337c29d20c4431
RahulPatil <rahul-rohidas.patil@informatik.hs-fulda.de>
2024-04-02 21:43:02 +0200

    Added Loading Spinner

---
+++ frontend/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <main className="flex justify-center items-center w-screen h-screen">
+            <div className="w-[67vmin] h-[67vmin] rounded-full border-t-2 border-white animate-spin"></div>
+        </main>
+    )
+}
