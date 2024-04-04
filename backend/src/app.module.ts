17ca91082a26a222351099745c079edd8616f6f4
RahulRohidasPatil <rahulrohidaspatil@gmail.com>
2024-04-04 23:12:39 +0200

    Added AuthModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
